d90b263bbd684c1228597a2612ca8d22d14fc326
Dagm Tesfu <lenovotablety847@gmail.com>
2026-05-11 18:13:35 +0300

    Set up Express backend for applications

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+let application = [];
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", function(req, res){
+    res.send("Internship Backend is Running");
+});
+
+app.get("/api/application", function(req, res){
+    res.json(application);
+});
+
+app.post("/api/application", function(req, res){
+    const newApp = {
+        id:Date.now(),
+        company: req.body.company,
+        position: req.body.position,
+        deadline: req.body.deadline,
+        description: req.body.description,
+        status: req.body.status
+    };
+
+    application.push(newApp);
+    res.status(201).json(newApp);
+})
+
+app.listen(PORT, function(){
+    console.log(`Server is Running on http://localhost:${PORT}`);
+});
